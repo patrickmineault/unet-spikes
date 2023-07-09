a5ad96e344c0b5541b71f3fe554a44334f0da1de
Patrick Mineault <patrick.mineault@gmail.com>
2023-07-08 20:51:42 -0400

    Add test for mask

--- tests/test_mask.py
+++ tests/test_mask.py
@@ -6,11 +6,12 @@ from src.mask import Masker, MaskParams
 def test_mask():
     masker = Masker(MaskParams(), "cpu")
     dummy = torch.zeros(1, 29, 49)
-    dummy[0, 0, 0] = 1
+    dummy[0, 0, :] = 1
     dummy = dummy.to(torch.long)
     _, data_masked = masker.mask_batch(dummy)
     assert data_masked.shape == dummy.shape
-    cases = torch.mean(data_masked.to(torch.float).squeeze(), axis=0)
+    data_masked = data_masked[:, 1:, :]
+    cases = torch.mean(data_masked.to(torch.float).squeeze(), axis=0)  # type: ignore
     print(data_masked[0, :, :])
     nbinary = ((cases == -100) | (cases == 0)).sum()
     assert nbinary == dummy.shape[2]
