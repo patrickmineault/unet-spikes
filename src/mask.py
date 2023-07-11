0d6d60be04600a7152b24f94e4a9b941ec347abb
Patrick Mineault <patrick.mineault@gmail.com>
2023-07-11 12:43:16 -0400

    Make the masking code simpler

--- src/mask.py
+++ src/mask.py
@@ -4,186 +4,34 @@
 from dataclasses import dataclass
 from enum import Enum
 
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-# Some infeasibly high spike count
-DEFAULT_MASK_VAL = 30
-UNMASKED_LABEL = -100
 
 class MaskMode(Enum):
-    full = "full"
     neuron = "neuron"
     timestep = "timestep"
-    timestep_only = "timestep_only"
 
 
-@dataclass
-class MaskParams:
-    MASK_RATIO: float = 0.2
-    MASK_TOKEN_RATIO: float = 1.0
-    MASK_RANDOM_RATIO: float = 0.5
-    MASK_MODE: MaskMode = MaskMode.timestep
-    MASK_MAX_SPAN: int = 1
-    MASK_SPAN_RAMP_START: int = 600
-    MASK_SPAN_RAMP_END: int = 1200
-    USE_ZERO_MASK: bool = True
-
-
-# Use a class so we can cache random mask
-class Masker:
-    def __init__(self, train_cfg: MaskParams, device: str):
-        self.update_config(train_cfg)
-        if self.cfg.MASK_MODE not in MaskMode:
-            raise Exception(
-                f"Given {self.cfg.MASK_MODE} not in supported {MaskMode}"
-            )
-        self.device = device
-
-    def update_config(self, config):
-        self.cfg = config
-        self.prob_mask = None
-
-    def expand_mask(self, mask, width):
-        r"""
-        args:
-            mask: N x T
-            width: expansion block size
-        """
-        kernel = torch.ones(width, device=mask.device).view(1, 1, -1)
-        expanded_mask = F.conv1d(mask.unsqueeze(1), kernel, padding=width // 2).clamp_(
-            0, 1
-        )
-        if width % 2 == 0:
-            expanded_mask = expanded_mask[
-                ..., :-1
-            ]  # crop if even (we've added too much padding)
-        return expanded_mask.squeeze(1)
-
-    def mask_batch(
-        self,
-        batch,
-        mask=None,
-        max_spikes=DEFAULT_MASK_VAL - 1,
-        should_mask=True,
-        expand_prob=0.0,
-        heldout_spikes=None,
-        forward_spikes=None,
+class Masker(nn.Module):
+    def __init__(
+        self, mask_mode: MaskMode = MaskMode.timestep, mask_ratio: float = 0.2
     ):
-        r"""Given complete batch, mask random elements and return true labels separately.
-        Modifies batch OUT OF place!
-        Modeled after HuggingFace's `mask_tokens` in `run_language_modeling.py`
-        args:
-            batch: batch NxTxH
-            mask: Optional mask to use. If mask is used, overried any internal masking
-            max_spikes: in case not zero masking, "mask token"
-            should_mask: whether the function should actually mask the data or only *generate* the mask
-            expand_prob: with this prob, uniformly expand. else, keep single tokens. UniLM does, with 40% expand to fixed, else keep single.
-            mask_ratio: ratio to randomly mask
-            heldout_spikes: None
-        returns:
-            batch: list of data batches NxTxH, with some elements along H set to -1s (we allow peeking between rates)
-            labels: true data (also NxTxH)
-        """
-        # We use standard BCT order, the function was written with BTC order in mind.
-        batch = (
-            batch.clone().permute(0, 2, 1).contiguous()
-        )
+        super(Masker, self).__init__()
 
-        mode = self.cfg.MASK_MODE
-        should_expand = (
-            self.cfg.MASK_MAX_SPAN > 1
-            and expand_prob > 0.0
-            and torch.rand(1).item() < expand_prob
-        )
-        width = (
-            torch.randint(1, self.cfg.MASK_MAX_SPAN + 1, (1,)).item()
-            if should_expand
-            else 1
-        )
-        mask_ratio = self.cfg.MASK_RATIO if width == 1 else self.cfg.MASK_RATIO / width
+        self.mask_mode = mask_mode
+        self.mask_ratio = mask_ratio
 
-        labels = batch.clone()
-        if mask is None:
-            if self.prob_mask is None or self.prob_mask.size() != labels.size():
-                if mode == MaskMode.full:
-                    mask_probs = torch.full(labels.shape, mask_ratio)
-                elif mode == MaskMode.timestep:
-                    single_timestep = labels[:, :, 0]  # N x T
-                    mask_probs = torch.full(single_timestep.shape, mask_ratio)
-                elif mode == MaskMode.neuron:
-                    single_neuron = labels[:, 0]  # N x H
-                    mask_probs = torch.full(single_neuron.shape, mask_ratio)
-                elif mode == MaskMode.timestep_only:
-                    single_timestep = labels[0, :, 0]  # T
-                    mask_probs = torch.full(single_timestep.shape, mask_ratio)
-                self.prob_mask = mask_probs.to(self.device)
-            # If we want any tokens to not get masked, do it here (but we don't currently have any)
-            mask = torch.bernoulli(self.prob_mask)
-
-            # N x T
-            if width > 1:
-                mask = self.expand_mask(mask, width)
-
-            mask = mask.bool()
-            if mode == MaskMode.timestep:
-                mask = mask.unsqueeze(2).expand_as(labels)
-            elif mode == MaskMode.neuron:
-                mask = mask.unsqueeze(1).expand_as(labels)
-            elif mode == MaskMode.timestep_only:
-                mask = mask.unsqueeze(0).unsqueeze(2).expand_as(labels)
-                # we want the shape of the mask to be T
-        elif mask.size() != labels.size():
-            raise Exception(
-                f"Input mask of size {mask.size()} does not match input size {labels.size()}"
-            )
-
-        # Here mask corresponds to non-masked labels
-        labels[
-            ~mask
-        ] = UNMASKED_LABEL  # No ground truth for unmasked - use this to mask loss
-        if not should_mask:
-            # Only do the generation
-            return batch.permute(0, 2, 1).contiguous(), labels.permute(0, 2, 1).contiguous()
-
-        # We use random assignment so the model learns embeddings for non-mask tokens, and must rely on context
-        # Most times, we replace tokens with MASK token
-        indices_replaced = (
-            torch.bernoulli(
-                torch.full(labels.shape, self.cfg.MASK_TOKEN_RATIO, device=mask.device)
-            ).bool()
-            & mask
-        )
-        if self.cfg.USE_ZERO_MASK:
-            batch[indices_replaced] = 0
-        else:
-            batch[indices_replaced] = max_spikes + 1
-
-        # Random % of the time, we replace masked input tokens with random value (the rest are left intact)
-        indices_random = (
-            torch.bernoulli(
-                torch.full(labels.shape, self.cfg.MASK_RANDOM_RATIO, device=mask.device)
-            ).bool()
-            & mask
-            & ~indices_replaced
-        )
-        random_spikes = torch.randint(
-            batch.max(), labels.shape, dtype=torch.long, device=batch.device
-        )
-        batch[indices_random] = random_spikes[indices_random]
-
-        if heldout_spikes is not None:
-            # heldout spikes are all masked
-            batch = torch.cat(
-                [batch, torch.zeros_like(heldout_spikes, device=batch.device)], -1
-            )
-            labels = torch.cat([labels, heldout_spikes.to(batch.device)], -1)
-        if forward_spikes is not None:
-            batch = torch.cat(
-                [batch, torch.zeros_like(forward_spikes, device=batch.device)], 1
-            )
-            labels = torch.cat([labels, forward_spikes.to(batch.device)], 1)
-        # Leave the other 10% alone
-        return batch.permute(0, 2, 1).contiguous(), labels.permute(0, 2, 1).contiguous()
+    def forward(self, X):
+        assert X.ndim == 3
+        if self.mask_mode == MaskMode.neuron:
+            # We mask one neuron at a time
+            # Neurons are along dimension 1
+            mask = torch.rand(1, X.shape[1], 1) < self.mask_ratio
+            return torch.expand_copy(mask, X.shape)
+        elif self.mask_mode == MaskMode.timestep:
+            # We mask one timestep at a time
+            # Timesteps are along dimension 2
+            mask = torch.rand(1, 1, X.shape[2]) < self.mask_ratio
+            return torch.expand_copy(mask, X.shape)
--- tests/test_mask.py
+++ tests/test_mask.py
@@ -1,38 +1,38 @@
 import pytest
 import torch
 
-from src.mask import Masker, MaskParams, MaskMode, UNMASKED_LABEL
+from src.mask import Masker, MaskMode
+
 
 def test_rel_qty():
     """Check that the relative quantity of masked tokens and replaced
     tokens is as expected."""
-    masker = Masker(MaskParams(MASK_RATIO=0.2), "cpu")
+    masker = Masker(MaskMode.neuron, 0.2)
     dummy = torch.zeros(1, 29, 49)
     dummy[0, 0, :] = 1
     dummy = dummy.to(torch.long)
-    batch, data_masked = masker.mask_batch(dummy)
-    percentage_same = (1.0 * (dummy == batch)).mean()
-    assert .95 < percentage_same < 1.0
-    percentage_masked = (1.0 * (data_masked != UNMASKED_LABEL)).mean()
-    assert 0.1 < percentage_masked < .35
+    mask = masker(dummy)
+    percentage_masked = (1.0 * (mask == 1)).mean()
+    assert 0.1 < percentage_masked < 0.35
+
 
 def test_dimension():
     """Check that the masking is taken along the expected dimensions."""
     for dim_type in [MaskMode.timestep, MaskMode.neuron]:
-        masker = Masker(MaskParams(MASK_MODE=dim_type), "cpu")
+        masker = Masker(dim_type, 0.2)
         dummy = torch.zeros(2, 29, 49)
         dummy[0, 0, :] = 1
         dummy = dummy.to(torch.long)
-        _, data_masked = masker.mask_batch(dummy)
-        assert data_masked.shape == dummy.shape
-        data_masked = data_masked[0, 1:, :]
+        the_mask = masker(dummy)
+        assert the_mask.shape == dummy.shape
+        the_mask = the_mask[0, 1:, :]
 
         if dim_type == MaskMode.timestep:
             # Check that masking is along the right dimension
-            cases = torch.mean(data_masked.to(torch.float).squeeze(), axis=0)  # type: ignore
-            nbinary = ((cases == -100) | (cases == 0)).sum()
+            cases = torch.mean(the_mask.to(torch.float).squeeze(), axis=0)  # type: ignore
+            nbinary = ((cases == 1) | (cases == 0)).sum()
             assert nbinary == dummy.shape[2]
         elif dim_type == MaskMode.neuron:
-            cases = torch.mean(data_masked.to(torch.float).squeeze(), axis=1)  # type: ignore
-            nbinary = ((cases == -100) | (cases == 0)).sum()
+            cases = torch.mean(the_mask.to(torch.float).squeeze(), axis=1)  # type: ignore
+            nbinary = ((cases == 1) | (cases == 0)).sum()
             assert nbinary == dummy.shape[1] - 1
